e2c89002c75525dc40f4b73942c026ead0222c45
Rohitesh Dutta <rohitesh.dutta@wingify.com>
2023-06-02 14:12:17 +0530

    fix(update): Update latest code from github to stash

--- VWOSdk.DemoApp/Controllers/HomeController.cs
+++ VWOSdk.DemoApp/Controllers/HomeController.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using VWOSdk.DemoApp.Models;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace VWOSdk.DemoApp.Controllers
 {
+
     public class HomeController : Controller
     {
         private static string GetRandomName()
@@ -24,14 +28,26 @@ namespace VWOSdk.DemoApp.Controllers
                };
 
         private static Settings SettingsFile { get; set; }
-        private static IVWOClient VWOClient { get; }
-
+        private static IVWOClient VWOClient { get; set; }
         static HomeController()
         {
             VWO.Configure(LogLevel.DEBUG);
             VWO.Configure(new CustomLogger());
+            CustomLogger logger = new CustomLogger();
             SettingsFile = SettingsProvider.GetSettingsFile(VWOConfig.SDK.AccountId, VWOConfig.SDK.SdkKey);
-            VWOClient = VWO.Launch(SettingsFile, isDevelopmentMode: false, userStorageService: new UserStorageService());
+            BatchEventData _batchData = new BatchEventData();
+            _batchData.EventsPerRequest = Defaults.EventsPerRequest;
+            _batchData.RequestTimeInterval = Defaults.RequestTimeInterval;
+            _batchData.FlushCallback = new FlushCallback();
+            //logger.WriteLog(LogLevel.DEBUG, "BatchEventData : EventsPerRequest-" + Defaults.EventsPerRequest.ToString() +", RequestTimeInterval:" + Defaults.RequestTimeInterval);          
+            //VWOClient = VWO.Launch(SettingsFile, batchData: _batchData);
+
+            //logger.WriteLog(LogLevel.DEBUG, "HookManager : IntegrationEventListener onEvent requested ");
+            //VWOClient = VWO.Launch(SettingsFile, batchData: _batchData, integrations: new HookManager(){HookCallback = new HookCallback()});
+
+            logger.WriteLog(LogLevel.DEBUG, "BatchEventData,userStorageService,isDevelopmentMode,integrations,shouldTrackReturningUser passed in SDK");
+            VWOClient = VWO.Launch(SettingsFile, batchData: _batchData, userStorageService: new UserStorageService(),
+                isDevelopmentMode: false, integrations: new HookManager() { HookCallback = new HookCallback() }, shouldTrackReturningUser: false);
         }
 
         [HttpGet]
@@ -39,7 +55,6 @@ namespace VWOSdk.DemoApp.Controllers
         {
             return View();
         }
-
         [HttpGet]
         public IActionResult Ab([FromQuery] string user)
         {
@@ -49,8 +64,10 @@ namespace VWOSdk.DemoApp.Controllers
             var options = VWOConfig.ABCampaignSettings.Options;
             string activateResponse = null, getVariationResponse = null;
             bool trackResponse = false;
+
             if (VWOClient != null)
             {
+
                 activateResponse = VWOClient.Activate(CampaignKey, userId, options);
                 getVariationResponse = string.IsNullOrEmpty(activateResponse) ? activateResponse : VWOClient.GetVariationName(CampaignKey, userId, options);
                 // Track all campaigns -
@@ -58,10 +75,12 @@ namespace VWOSdk.DemoApp.Controllers
 
                 // Track multiple campaigns -
                 // VWOClient.Track(new List<String>(){ CampaignKey, "Test2" }, userId, goalIdentifier, options);
+
                 trackResponse = string.IsNullOrEmpty(activateResponse) ? false : VWOClient.Track(CampaignKey, userId, goalIdentifier, options);
             }
             var json = new ViewModel(SettingsFile, userId, CampaignKey, goalIdentifier, activateResponse, getVariationResponse, trackResponse, options);
             return View(json);
+
         }
 
 
@@ -102,8 +121,9 @@ namespace VWOSdk.DemoApp.Controllers
             {
 
                 activateResponse = VWOClient.IsFeatureEnabled(CampaignKey, userId, options);
-                if (activateResponse) {
-                  VWOClient.Track(CampaignKey, userId, goalIdentifier, options);
+                if (activateResponse)
+                {
+                    VWOClient.Track(CampaignKey, userId, goalIdentifier, options);
                 }
                 stringVariable = VWOClient.GetFeatureVariableValue(CampaignKey, stringVariableKey, userId, options);
                 integerVariable = VWOClient.GetFeatureVariableValue(CampaignKey, integerVariableKey, userId, options);
@@ -128,5 +148,71 @@ namespace VWOSdk.DemoApp.Controllers
             var json = new ViewModel(SettingsFile, userId, activateResponse, tagKey, tagValue);
             return View(json);
         }
+        [HttpGet]
+        public IActionResult Flush()
+        {
+
+            if (VWOClient != null)
+            {
+                CustomLogger logger = new CustomLogger();
+                logger.WriteLog(LogLevel.DEBUG, "manual flushEvent called ");
+                bool response = VWOClient.FlushEvents();
+                logger.WriteLog(LogLevel.DEBUG, "flushEvent response: " + response.ToString());
+            }
+            return View("Index");
+        }
+        [Route("/webhook")]
+        [HttpPost]
+        public async Task<string> webhook()
+        {
+            CustomLogger logger = new CustomLogger();
+            string PayLoad;
+            logger.WriteLog(LogLevel.DEBUG, "Post request from vwo app");
+
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                PayLoad = await reader.ReadToEndAsync();
+            }
+            logger.WriteLog(LogLevel.DEBUG, "VWO webhook payload: " + PayLoad);
+            if (string.IsNullOrEmpty(Defaults.WebhookSecretKey) == false)
+            {
+                logger.WriteLog(LogLevel.DEBUG, "WebhookSecretKey exists . VWO webhook authentication Checking.");
+
+                if (Request.Headers["x-vwo-auth"].ToString() != Defaults.WebhookSecretKey)
+                {
+                    logger.WriteLog(LogLevel.DEBUG, "VWO webhook authentication failed. Please check.");
+                    return "VWO webhook authentication failed. Please check.";
+                }
+
+                if (VWOClient != null)
+                {
+                    logger.WriteLog(LogLevel.DEBUG, "Authentication passed and GetAndUpdateSettingsFile function is called");
+                    await SettingsProvider.GetAndUpdateSettingsFile(VWOConfig.SDK.AccountId, VWOConfig.SDK.SdkKey);
+                    logger.WriteLog(LogLevel.DEBUG, "Setting file has been updated");
+                }
+
+
+            }
+            else
+            {
+
+
+                if (VWOClient != null)
+                {
+                    logger.WriteLog(LogLevel.DEBUG, "GetAndUpdateSettingsFile function called");
+                    await SettingsProvider.GetAndUpdateSettingsFile(VWOConfig.SDK.AccountId, VWOConfig.SDK.SdkKey);
+                    logger.WriteLog(LogLevel.DEBUG, "Setting file has been updated");
+
+                }
+
+
+            }
+
+
+            return "";
+        }
+
     }
+
+
 }
---
+++ VWOSdk.DemoApp/Models/HookCallback.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+namespace VWOSdk.DemoApp
+{
+    internal class HookCallback : IntegrationEventListener
+    {     
+        public void onEvent(Dictionary<string, dynamic> properties)
+        {
+            string payLoad = JsonConvert.SerializeObject(properties);
+            CustomLogger logger = new CustomLogger();
+            logger.WriteLog(LogLevel.DEBUG, "onEvent call from SDK: " + payLoad);
+
+        }
+    }
+}
